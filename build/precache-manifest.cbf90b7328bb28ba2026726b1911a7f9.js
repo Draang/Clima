35ef7f93625cf94bd528e9b7b1fe89f71e1dd631
Angel Contreras Dueñas <c3po.angel@gmail.com>
2020-07-29 13:49:11 -0600

    Esto es todo amigos

---
+++ build/precache-manifest.cbf90b7328bb28ba2026726b1911a7f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b54f5530409724ceefb3ab0089ef3dac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9026409ab99068f87d20",
+    "url": "/static/css/main.3e82f7ce.chunk.css"
+  },
+  {
+    "revision": "ece6a26b491f477efa18",
+    "url": "/static/js/2.5adef9fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5adef9fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9026409ab99068f87d20",
+    "url": "/static/js/main.99e58195.chunk.js"
+  },
+  {
+    "revision": "f697e06b72e1609d0ac2",
+    "url": "/static/js/runtime-main.36419111.js"
+  }
+]);
